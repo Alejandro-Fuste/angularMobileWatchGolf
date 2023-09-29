a4a3eb27a0c137b2e4acca9cc176b6764061c067
Alejandro F <afuste7@gmail.com>
2023-09-29 16:18:07 -0400

    added sample navigation with sample components

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { HomeScreen, ProfileScreen } from "./src/components";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>It works!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      {/* <View style={styles.container}>
+        <Text>It works! Hello Word!</Text>
+        <StatusBar style="auto" />
+      </View> */}
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Welcome" }}
+        />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
